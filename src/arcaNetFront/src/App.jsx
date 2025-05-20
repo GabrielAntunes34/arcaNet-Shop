6c2519f8865162a06dc3381f33234b038b66fa27
GabrielAntunes34 <antunes.gabriel.345@gmail.com>
2025-05-19 21:09:23 -0300

    Front End architecture with React

---
+++ src/arcaNetFront/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>MILESTONE 2!!!!!!!!!!!!!!</h1>
+      <p>And so it begins. 19/05/2025</p>
+      <small><p>10 days remaining</p></small>
+    </>
+  );
+}
+
+export default App
