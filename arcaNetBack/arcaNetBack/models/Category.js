f7c6d3cf0a3a1dcc7ff52a821da9400f036fb983
GabrielAntunes34 <antunes.gabriel.345@gmail.com>
2025-06-23 00:30:39 -0300

    Removendo docker em uma branch separada para testes

--- arcaNetBack/arcaNetBack/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  status: { type: String, enum: ['Active', 'Inactive'], default: 'Active' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Category', categorySchema);
--- arcaNetBack/arcaNetFront/src/services/authServices.js
+++
@@ -1,4 +0,0 @@
-const apiUrl = import.meta.env.API_BASE_URL;
-console.log(apiUrl);
-
-export default apiUrl;
